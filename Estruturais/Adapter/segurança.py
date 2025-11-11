d65e6943767a1da884e4a37a5393189b835dab9f
Gabriel Martins <159476031+GabrielGreapy@users.noreply.github.com>
2025-11-11 18:12:23 +0000

    Adapter feito

--- Estruturais/Adapter/segurança.py
+++ Estruturais/Adapter/segurança.py
@@ -8,8 +8,14 @@ class voce_adaptee:
     
 
 class tradutor_adapter:
+    def __init__(self, adaptee : "voce_adaptee"):
+        self._adaptee = adaptee
     def request(self) -> str:
-        return f"Tradutor(Adapter): (Traduzido) {self.specific_request()[::-1]}"
+        dado_invertido = self._adaptee.specific_request()
+        dado_traduzido = dado_invertido[::-1]
+        return f"Tradutor(Adapter): (Traduzido) {dado_traduzido}"
+
+    
     
 
 def seguranca_alfabetizado(seguranca_target : "seguranca_target") -> None:
@@ -18,15 +24,15 @@ def seguranca_alfabetizado(seguranca_target : "seguranca_target") -> None:
 
 if __name__ == "__main__":
     print("Opa, nesse universo um temos segurança alfabetizado, é um universo que focaram" \
-    "na educação e foi efetio.")
+    "na educação e foi efetivo.")
     target = seguranca_target()
     seguranca_alfabetizado(target)
     adaptee = voce_adaptee()
     print("Mas aqui estamos na realidade(imaginada), o segurança não consegue ler o que está escrito no meu braço. \n agora precisamos" \
     "de um tradutor mas veja como não consigo")
-    print(f"Adaptee : {voce_adaptee.specific_request()}", end ="  ")
+    print(f"Adaptee : {adaptee.specific_request()}", end ="  ")
     print("Adaptee: mas eu consigo se alguem ler pra mim")
-    adapter = tradutor_adapter()
+    adapter = tradutor_adapter(adaptee)
     adapter.request()
     seguranca_alfabetizado(adapter)
 
